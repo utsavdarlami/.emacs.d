19326c19b78039ce6bb5a2cc598d0ac03b1fbb9d
Utsav Darlami <utsavdarlami17@gmail.com>
2021-12-17 22:11:13 +0545

    Initial commit

---
+++ adopt.sh
@@ -0,0 +1,9 @@
+if [ $1 = "yes" ]; then
+    # -nvt for simulation ,, to check if everythings is safely linked
+    stow --adopt -vt ~/.emacs.d . 
+    stow --adopt -vt ~/Documents/org-notes/templates/ templates
+else
+    #stow -vt for actual linking
+    stow --adopt -nvt ~/.emacs.d . 
+    stow --adopt -nvt ~/Documents/org-notes/templates/ templates
+fi
