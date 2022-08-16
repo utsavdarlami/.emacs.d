3f9f078cd2ced22300de36ea3f2d76f3c7a98594
utsavdarlami <utsavdarlami17@gmail.com>
2022-08-16 06:51:29 +0545

    Add: ya-snippet bindings and changed distro

--- adopt.sh
+++ adopt.sh
@@ -2,9 +2,9 @@ if [ $1 == 'yes' ]
 then
     #stow -vt for actual linking
     stow --adopt -vt ~/.emacs.d . 
-    stow --adopt -vt ~/Documents/org-notes/templates/ templates
+    stow --adopt -vt ~/Dropbox/org-notes/templates/ templates
 else
     # -nvt for simulation ,, to check if everythings is safely linked
     stow --adopt -nvt ~/.emacs.d . 
-    stow --adopt -nvt ~/Documents/org-notes/templates/ templates
+    stow --adopt -nvt ~/Dropbox/org-notes/templates/ templates
 fi
